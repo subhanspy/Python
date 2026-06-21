0a7ee5e5955b4596a608d8d7cb7bff1100658961
subhanspy <subhanrashid.7506@gmail.com>
2026-06-21 11:01:35 -0700

    Functions

--- prac4.py
+++ prac4.py
@@ -1,4 +1,5 @@
 #Functions in Python
+
 def multiply(x,y):
     return x*y
 
@@ -10,15 +11,45 @@ def addition(a, b, c, d=10):
 
 print(addition(c=10, a=10, b=10))
 
-def divide(numerator, denominator):
-    if denominator == 0:
-        return "Error: Cannot divide by zero."
-    return numerator / denominator
+def divide (numerator,denominator):
+    if numerator==0:
+        return  "Error: Cannot divide by zero"
+    if denominator==0:
+        return "Error: Enter other numbers"
+    return numerator/denominator
 
-# Correct usage
-result = divide(10, 2)  
-print(result)  
+Answer = divide (12,3)
+print(Answer)
 
-# Incorrect order
-result = divide(denominator=2, numerator=10)  
-print(result) 
+# Keywords Arguments
+
+def person(age,name,city):
+    return f"{name} is {age} years old and lives in {city}"
+
+profile = person(age=22,name="Subhan",city="Nankana sahib")
+print(profile)
+
+#Variable Length Arguments
+
+def vari_length(*args):
+    return " ".join(args)
+
+concatenate = vari_length("My","Name","is","subhan")
+print(concatenate)
+
+# by itration :
+def concatenation(*args):
+    for i in args:
+        print (" ",i)
+
+seq = concatenation("Ali",7,33.4,"Ahmad")
+
+def addition (*args):
+    total = 0
+    for i in args:
+        total+=i
+        
+    return total
+
+add = addition(10,20,30,40,10)
+print(add)    
