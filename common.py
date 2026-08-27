44e702d701d021dad63c7eb12c6c6fe304577673
subhanspy <subhanrashid.7506@gmail.com>
2026-08-27 04:54:41 -0700

    Slicing & Indexing

---
+++ common.py
@@ -0,0 +1,20 @@
+
+s="Hello World"
+
+print(s[1])
+
+print(s[0:3])
+
+print(s[::4])
+print(s[::-1])
+
+
+d={"Name":"ahamd","Age":18}
+print(d["Age"])
+print(d["Name"])
+#Flexible dictionary
+d={"dic_1":123,"dic_2":[1,2,3,4],"dic_3":["item1","item2","item3"]}
+print(d["dic_2"])
+print(d["dic_3"][1])
+
+print(d["dic_3"][0].upper())
